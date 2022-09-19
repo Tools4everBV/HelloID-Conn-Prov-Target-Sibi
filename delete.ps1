823982c490dad9c7eb36a7f5bb8dc99a03601a76
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2022-09-19 16:07:30 +0200

    Create delete.ps1

---
+++ delete.ps1
@@ -0,0 +1,151 @@
+#####################################################
+# HelloID-Conn-Prov-Target-Sibi-Delete
+#
+# Version: 1.0.0
+#####################################################
+# Initialize default values
+$config = $configuration | ConvertFrom-Json
+$p = $person | ConvertFrom-Json
+$aRef = $AccountReference | ConvertFrom-Json
+$success = $false
+$auditLogs = [System.Collections.Generic.List[PSCustomObject]]::new()
+
+# Account mapping
+$account = [PSCustomObject]@{
+    'employment_end' = (Get-Date).ToString('yyyy-MM-dd')
+}
+
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+# Set debug logging
+switch ($($config.IsDebug)) {
+    $true { $VerbosePreference = 'Continue' }
+    $false { $VerbosePreference = 'SilentlyContinue' }
+}
+
+#region functions
+function Resolve-SibiError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [object]
+        $ErrorObject
+    )
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            ScriptLineNumber = $ErrorObject.InvocationInfo.ScriptLineNumber
+            Line             = $ErrorObject.InvocationInfo.Line
+            ErrorDetails     = ''
+            FriendlyMessage  = ''
+        }
+        if ($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') {
+            $httpErrorObj.ErrorDetails = $ErrorObject.ErrorDetails.Message
+            $httpErrorObj.FriendlyMessage = ($ErrorObject.ErrorDetails.Message | ConvertFrom-Json).Message
+        } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            if ($null -eq $ErrorObject.Exception.Response) {
+                $httpErrorObj.ErrorDetails = $ErrorObject.Exception.Message
+                $httpErrorObj.FriendlyMessage = $ErrorObject.Exception.Message
+            } else {
+                $streamReaderResponse = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
+                $httpErrorObj.ErrorDetails = $streamReaderResponse
+                $httpErrorObj.FriendlyMessage = ($streamReaderResponse | ConvertFrom-Json).Message
+            }
+        }
+        Write-Output $httpErrorObj
+    }
+}
+#endregion
+
+# Begin
+try {
+    Write-Verbose 'Adding authorization headers'
+    $headers = [System.Collections.Generic.Dictionary[string, string]]::new()
+    $headers.Add("Authorization", "Bearer $($config.Token)")
+    $headers.Add("Accept", "application/json")
+
+    # Verify if Sibi account exists
+    try {
+        Write-Verbose "Verifying if Sibi account for [$($p.DisplayName)] exists"
+        $splatParams = @{
+            Uri         = "$($config.BaseUrl)/api/employees/get/by-en/$aRef2222"
+            Method      = 'GET'
+            Headers     = $headers
+            ContentType = 'application/json'
+        }
+        $responseUser = Invoke-RestMethod @splatParams
+        if ($responseUser){
+            $action = 'Found'
+            $dryRunMessage = "Delete Sibi account for: [$($p.DisplayName)] will be executed during enforcement"
+            Write-Verbose $dryRunMessage
+        }
+    } catch {
+        $ex = $PSItem
+        $errorObj = Resolve-SibiError -ErrorObject $ex
+        Write-Verbose $errorObj.ErrorDetails
+        if ($ex.Exception.Response.StatusCode -eq 'NotFound'){
+            $action = 'NotFound'
+            $dryRunMessage = "Sibi account for: [$($p.DisplayName)] not found. Possibily already deleted. Skipping action."
+            Write-Verbose $dryRunMessage
+        } else {
+            throw
+        }
+    }
+
+    # Add an auditMessage showing what will happen during enforcement
+    if ($dryRun -eq $true) {
+        Write-Warning = "[DryRun] $dryRunMessage"
+    }
+
+    # Process
+    if (-not($dryRun -eq $true)) {
+        switch ($action){
+            'Found'{
+                Write-Verbose "Deleting Sibi account with accountReference: [$aRef]"
+                $splatParams = @{
+                    Uri         = "$($config.BaseUrl)/api/employees/update/by-en/$aRef"
+                    Method      = 'PATCH'
+                    Headers     = $headers
+                    Body        = $account | ConvertTo-Json
+                    ContentType = 'application/json'
+                }
+                $null = Invoke-RestMethod @splatParams
+                $auditLogs.Add([PSCustomObject]@{
+                        Message = 'Delete account was successful'
+                        IsError = $false
+                    })
+            }
+
+            'NotFound'{
+                $auditLogs.Add([PSCustomObject]@{
+                    Message = "Sibi account for: [$($p.DisplayName)] not found. Possibily already deleted. Skipping action."
+                    IsError = $false
+                })
+            }
+        }
+        $success = $true
+    }
+} catch {
+    $success = $false
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-SibiError -ErrorObject $ex
+        $auditMessage = "Could not delete Sibi account. Error: $($errorObj.FriendlyMessage)"
+        Write-Verbose "Error at Line '$($errorObj.ScriptLineNumber)': $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
+    } else {
+        $auditMessage = "Could not delete Sibi account. Error: $($ex.Exception.Message)"
+        Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+    }
+    $auditLogs.Add([PSCustomObject]@{
+            Message = $auditMessage
+            IsError = $true
+        })
+# End
+} finally {
+    $result = [PSCustomObject]@{
+        Success   = $success
+        Auditlogs = $auditLogs
+    }
+    Write-Output $result | ConvertTo-Json -Depth 10
+}
